c069abe90cc7b226abe26b91bf9d1b17716b9c6d
Dariell Vasquez <dariell@Dariells-MacBook-Air.local>
2017-10-28 10:04:27 -0400

    Finished main spotify functionality

--- liri.js
+++ liri.js
@@ -1,12 +1,12 @@
 var keys = require("./keys.js");
 var twitterModule = require("./twitterModule");
-var spotifyModule = require("./twitterModule");
+// var spotifyModule = require("./twitterModule");
 // var inspector = require("inspect-process");
 
-var args = process.argv[2];
+var firstArg = process.argv[2];
 // console.log(args);
 debugger;
-switch (args) {
+switch (firstArg) {
     case "my-tweets":
         // console.log("tweet");
         twitterModule.tweet();
@@ -27,3 +27,44 @@ switch (args) {
     default:
         console.log("nope");
 }
+
+var userInput = "";
+for (var i= 3; i < process.argv.length; i++ ){
+    userInput += process.argv[i] + " ";
+}
+
+var noSong = "The Sign, Ace of Base";
+
+
+
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify({
+ id: "b1982e786b3c4df0b48dbd24bbf7f759",
+ secret: "627111d972a64ad6adb0dd7ff3de9e7d"
+});
+
+spotify.search({ type: 'track', query: userInput }, function(err, data) {
+ if (err) {
+   return console.log('Error occurred: ' + err);
+ }
+ debugger;
+console.log(data.tracks.items[0].artists[0].name); 
+console.log(data.tracks.items[0].name);
+console.log(data.tracks.items[0].external_urls);
+console.log(data.tracks.items[0].album.name);
+});
+
+
+// This will show the following information about the song in your terminal/bash window
+
+// Artist(s)
+
+// The song's name
+
+// A preview link of the song from Spotify
+
+// The album that the song is from
+
+// If no song is provided then your program will default to "The Sign" by Ace of Base.
+
